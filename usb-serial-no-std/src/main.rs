45063ef26b61ad82605b9c085b86a2bd7b31012e
Preston Garrison III <preston.garrison3@gmail.com>
2024-05-02 18:52:18 -0400

    add usb serial jtag example

---
+++ usb-serial-no-std/src/main.rs
@@ -0,0 +1,76 @@
+#![no_std]
+#![no_main]
+
+use embassy_executor::Spawner;
+use embassy_sync::{blocking_mutex::raw::NoopRawMutex, signal::Signal};
+use esp_backtrace as _;
+use esp_hal::{
+    clock::ClockControl,
+    embassy,
+    peripherals::Peripherals,
+    prelude::*,
+    timer::TimerGroup,
+    usb_serial_jtag::{UsbSerialJtag, UsbSerialJtagRx, UsbSerialJtagTx},
+    Async,
+};
+use static_cell::StaticCell;
+
+const MAX_BUFFER_SIZE: usize = 512;
+
+#[embassy_executor::task]
+async fn writer(
+    mut tx: UsbSerialJtagTx<'static, Async>,
+    signal: &'static Signal<NoopRawMutex, heapless::String<MAX_BUFFER_SIZE>>,
+) {
+    use core::fmt::Write;
+    embedded_io_async::Write::write_all(
+        &mut tx,
+        b"Hello async USB Serial JTAG. Type something.\r\n",
+    )
+    .await
+    .unwrap();
+    loop {
+        let message = signal.wait().await;
+        signal.reset();
+        write!(&mut tx, "-- received '{}' --\r\n", message).unwrap();
+        embedded_io_async::Write::flush(&mut tx).await.unwrap();
+    }
+}
+
+#[embassy_executor::task]
+async fn reader(
+    mut rx: UsbSerialJtagRx<'static, Async>,
+    signal: &'static Signal<NoopRawMutex, heapless::String<MAX_BUFFER_SIZE>>,
+) {
+    let mut rbuf = [0u8; MAX_BUFFER_SIZE];
+    loop {
+        let r = embedded_io_async::Read::read(&mut rx, &mut rbuf).await;
+        match r {
+            Ok(len) => {
+                let mut string_buffer: heapless::Vec<_, MAX_BUFFER_SIZE> = heapless::Vec::new();
+                string_buffer.extend_from_slice(&rbuf[..len]).unwrap();
+                signal.signal(heapless::String::from_utf8(string_buffer).unwrap());
+            }
+            Err(e) => esp_println::println!("RX Error: {:?}", e),
+        }
+    }
+}
+#[main]
+async fn main(spawner: Spawner) {
+    let peripherals = Peripherals::take();
+    let system = peripherals.SYSTEM.split();
+
+    let clocks = ClockControl::max(system.clock_control).freeze();
+    let timg0 = TimerGroup::new_async(peripherals.TIMG0, &clocks);
+    embassy::init(&clocks, timg0);
+
+    let (tx, rx) = UsbSerialJtag::new_async(peripherals.USB_DEVICE).split();
+    esp_println::logger::init_logger_from_env();
+
+    static SIGNAL: StaticCell<Signal<NoopRawMutex, heapless::String<MAX_BUFFER_SIZE>>> =
+        StaticCell::new();
+    let signal = &*SIGNAL.init(Signal::new());
+
+    spawner.spawn(reader(rx, &signal)).unwrap();
+    spawner.spawn(writer(tx, &signal)).unwrap();
+}
